7506fcd0b261348ebb2fe5cf2f90a8124d02d688
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-04-12 09:29:03 +0200

    Add [hello] package

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     fpm.url = "git+ssh://git@github.com/fstar-package-manager/fpm";
+    hello = {flake = false; url = "git+ssh://git@github.com/fstar-package-manager/example-packages?dir=hello";};
   };
   outputs = { self, fpm }: {};
 }
